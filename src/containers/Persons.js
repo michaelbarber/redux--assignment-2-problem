346b9f48c878983bf26e0a5dba046f8df192e620
Mike Barber <michaelbarber.dev@gmail.com>
2020-12-27 21:59:08 +0000

    Assignment 5: Redux Basics. 269: Assignment Solution

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,44 +1,46 @@
 import React, { Component } from 'react';
+// connect is a function which takes a component as an input and returns a hoc (higher order component)
+import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
+import * as actionTypes from '../store/actions'
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
-
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
-
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
 
     render () {
         return (
             <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
+                <AddPerson personAdded={this.props.onAddedPerson} />
+                {this.props.prs.map(person => (
                     <Person 
                         key={person.id}
                         name={person.name} 
                         age={person.age} 
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        clicked={() => this.props.onRemovedPerson(person.id)}/>
                 ))}
             </div>
         );
     }
 }
 
-export default Persons;
+// store instructions of how you want the state managed by redux to be mapped to props which you will use
+const mapStateToProps = state => {
+    // map of prop names and slices of the state stored in redux
+    // it's our way of configuring which kind of information we need
+    return {
+        prs: state.persons
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddedPerson: () => dispatch({type: actionTypes.ADD_PERSON}),
+        onRemovedPerson: (id) => dispatch({type: actionTypes.REMOVE_PERSON, personId: id})
+    };
+};
+
+// how it goes 
+// 1. connect(apStateToProps, mapDispatchToProps) is the first function that is executed which returns a function
+// 2. the function that is returned takes in Persons as an argument and returns a hoc (higher order component)
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
---
+++ src/store/reducer.js
@@ -0,0 +1,34 @@
+import * as actionTypes from './actions';
+
+const initialStateForThisReducer = {
+    persons: [],
+}
+
+// this is just a function
+// we are assigning a function to a variable
+// state = initialState is just giving state a default value
+const reducer = (state = initialStateForThisReducer, action) => {
+
+    switch (action.type){
+        case actionTypes.ADD_PERSON:
+            const newPerson = {
+                id: Math.random(), // not really unique but good enough here!
+                name: 'Max',
+                age: Math.floor( Math.random() * 40 )
+            }
+            return {
+                ...state,
+                persons: state.persons.concat(newPerson)
+            }
+        case actionTypes.REMOVE_PERSON:
+            return {
+                ...state,
+                persons: state.persons.filter(person => person.id !== action.personId)
+            }
+    }
+    return state;
+};
+
+
+// so you can use the reducer outside of this file
+export default reducer;
